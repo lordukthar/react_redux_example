72dd6b03b4999cc01915288a04e5975522bd39ff
Jonas A <jonas@lhost.se>
2021-09-19 17:45:13 +0200

    adding phony header

--- src/components/library/Server/Server.js
+++ src/components/library/Server/Server.js
@@ -1,6 +1,10 @@
-import { ContentAddCircleOutline } from "material-ui/svg-icons";
-
 const axios = require("axios");
+
+const instance = axios.create({
+  // baseURL: 'https://jsonplaceholder.typicode.com/users/',
+  timeout: 1000,
+  headers: { "X-Custom-Header": "foobar" },
+});
 
 export default function getUsers() {
   return axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
@@ -9,12 +13,16 @@ export default function getUsers() {
 }
 
 export function getUser(id) {
-  return axios
-    .get("https://jsonplaceholder.typicode.com/users/" + id)
+  return instance
+    .get("https://jsonplaceholder.typicode.com/users/" + id, {})
     .then(function (response) {
       // handle success
       var a = response.data;
-      console.log(JSON.stringify(a));
+      console.log("response" + JSON.stringify(response));
+      console.log("data:" + JSON.stringify(response.data));
+      console.log(
+        "config:" + JSON.stringify(response.config.headers["X-Custom-Header"])
+      );
       return a;
     })
     .catch(function (error) {
@@ -23,3 +31,13 @@ export function getUser(id) {
       return { name: "No name", id: 0 };
     });
 }
+
+/**
+ * const {promisify} = require('util');
+const got = require('got');
+const {CookieJar} = require('tough-cookie');
+const cookieJar = new CookieJar();
+const setCookie = promisify(cookieJar.setCookie.bind(cookieJar));
+await setCookie(`cookie_name=${cookie_value}`, 'https://your-domain.com');
+await got('https://your-domain.com', {cookieJar} )
+ */
